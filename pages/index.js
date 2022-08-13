096e908ca5e3bbdc48d26a4f270298fe0cb713a8
HWOOD22 <hwood22@ford.com>
2022-08-13 19:40:11 -0400

    Done with login logout flow

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="bg-black h-screen overflow-hidden">
+        <Sidebar />
+      </main>
+    </div>
+  );
+}
