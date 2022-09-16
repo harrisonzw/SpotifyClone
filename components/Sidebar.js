9001387d7509cfd0279e19a040247f70015503cc
HWOOD22 <hwood22@ford.com>
2022-09-15 23:13:58 -0400

    Removed Firebase scripts

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
   HomeIcon,
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
   RssIcon,
-} from "@heroicons/react/outline";
-import { HeartIcon } from "@heroicons/react/solid";
-import { useSession } from "next-auth/react";
-import useSpotify from "../hooks/useSpotify";
-import { useRecoilState } from "recoil";
-import { playlistIdState } from "../atoms/playlistAtom";
+} from '@heroicons/react/outline';
+import { HeartIcon } from '@heroicons/react/solid';
+import { useSession } from 'next-auth/react';
+import useSpotify from '../hooks/useSpotify';
+import { useRecoilState } from 'recoil';
+import { playlistIdState } from '../atoms/playlistAtom';
 
 function Sidebar() {
   const spotifyApi = useSpotify();
@@ -28,43 +28,43 @@ function Sidebar() {
 
   return (
     <div
-      className="text-gray-500 p-5 text-xs lg:text-sm border-r 
+      className='text-gray-500 p-5 text-xs lg:text-sm border-r 
     border-gray-900 overflow-y-scroll scrollbar-hide h-screen
-     sm:max-w[12rem] lg:max-w-[15rem] hidden md:inline-flex"
+     sm:max-w[12rem] lg:max-w-[15rem] hidden md:inline-flex'
     >
-      <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5" />
+      <div className='space-y-4'>
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <HomeIcon className='h-5 w-5' />
           <p>Home</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="h-5 w-5" />
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <SearchIcon className='h-5 w-5' />
           <p>Search</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="h-5 w-5" />
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <LibraryIcon className='h-5 w-5' />
           <p>Your Library</p>
         </button>
-        <hr className="border-t-[0.1px] border-gray-900" />
+        <hr className='border-t-[0.1px] border-gray-900' />
 
-        <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="h-5 w-5" />
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <PlusCircleIcon className='h-5 w-5' />
           <p>Create Playlist</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5 text-blue-500" />
+        <button className='flex items-center space-x-2 hover:text-white'>
+          <HeartIcon className='h-5 w-5 text-blue-500' />
           <p>Liked Songs</p>
         </button>
-        <hr className="border-t-[0.1px] border-gray-900" />
+        <hr className='border-t-[0.1px] border-gray-900' />
 
         {/* playlists */}
         {playlists.map((playlist) => (
           <p
             key={playlist.id}
             onClick={() => setPlaylistId(playlist.id)}
-            className="cursor-pointer hover:text-white"
+            className='cursor-pointer hover:text-white'
           >
-            {playlist.name !== "Starred" ? playlist.name : ""}
+            {playlist.name !== 'Starred' && playlist.name}
           </p>
         ))}
       </div>
